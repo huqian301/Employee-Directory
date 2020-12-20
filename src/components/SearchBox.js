344b707b7abcb64ebea2f8449395ec42cd978d63
huqian301 <huqian301@gmail.com>
2020-12-20 15:22:42 -0800

    adding style for each components

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import "../styles/SearchBox.css";
+import "../styles/SearchBox.css";
 
 function SearchBox({ handleSearchChange }) {
   return (
     <div className="searchbox">
-      <form className="form-inline">
+      <form className="form-inline" >
         <input
           className="form-control"
           type="search"
